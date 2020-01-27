7f7a965f641b7fd481d394e4d5b12bd92bbcaf64
Student <fdfdfg@dfg.com>
2020-01-27 21:04:11 +0300

    init

---
+++ src/by/belhard/j20/lessons/lesson01/FirstMain.java
@@ -0,0 +1,11 @@
+package by.belhard.j20.lessons.lesson01;
+
+public class FirstMain {
+
+    public static void main(String[] args) {
+        /*        throw new NullPointerException(); // fewfwe*/
+
+        System.out.println("Hello world!!!111oneoneone");
+
+    }
+}
